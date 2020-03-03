c0488f451a22ac14f75c3bbdbb9c59fddc6a35e3
admin <admin@admins-Mac-mini-6.local>
2020-03-03 14:42:13 +0530

    My Git App

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by admin on 03/03/20.
+//  Copyright © 2020 MSCIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
